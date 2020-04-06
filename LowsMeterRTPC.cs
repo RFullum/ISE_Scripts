95333fa97885a99c9f1172065a7df273cce9202e
rfullum <rfullum@gmail.com>
2020-04-06 08:55:50 +0100

    Building Spectrum analyzers
    Setting up height cylinder for Lows

---
+++ LowsMeterRTPC.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Gets meter value from Wwise Subs Aux Bus
+ */
+public class LowsMeterRTPC : MonoBehaviour
+{
+    // Get PlayingID
+    uint playingID = (uint)AkQueryRTPCValue.RTPCValue_PlayingID;
+
+    // Instantiate meterValue variable
+    public static float meterValue;
+
+    // Game Object
+    [SerializeField] GameObject obj;
+
+
+
+    void Update()
+    {
+        updateMeter();
+    }
+
+    void updateMeter()
+    {
+        int type = 1;
+        AkSoundEngine.GetRTPCValue("LowsRTPC", obj, playingID, out meterValue, ref type);
+    }
+}
