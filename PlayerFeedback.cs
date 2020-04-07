8117808ff36fa27d113c3c273d145390c44b296b
rfullum <rfullum@gmail.com>
2020-04-07 19:06:04 +0100

    Update PlayerFeedback.cs
    Added H Button Feedback

--- PlayerFeedback.cs
+++ PlayerFeedback.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class PlayerFeedback : MonoBehaviour
 {
     [SerializeField] string bButtonFeedback;
+    [SerializeField] string hButtonFeedback;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.B))
             AkSoundEngine.PostEvent(bButtonFeedback, gameObject);
-
+        if (Input.GetKeyDown(KeyCode.H))
+            AkSoundEngine.PostEvent(hButtonFeedback, gameObject);
     }
 }
