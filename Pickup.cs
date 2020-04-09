9652e9c7e2a1ed69ccbc72790c7ab63fb5c46697
rfullum <rfullum@gmail.com>
2020-04-09 14:27:36 +0100

    Spawn and carry bass ball
    Shooting bass cylinder with B spawns bass ball. Pick up and drop bass ball into sequencer

---
+++ Pickup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        // Create a ray at center of mouse position (center screen)
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out var hit))
+        {
+            if(hit.collider.gameObject == gameObject)
+            {
+                GetComponent<Rigidbody>().useGravity = false;
+                gameObject.transform.parent = GameObject.Find("FirstPersonCharacter").transform;
+            }
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        gameObject.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+
+}
---
+++ SpawnBassBall.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/**
+ * Spawns ball to grab and put into bass sequencer
+ */
+public class SpawnBassBall : MonoBehaviour
+{
+    [SerializeField] string bassTag = "bassSeq";
+    [SerializeField] GameObject spawnee;
+
+    void Update()
+    {
+        // Create a ray at center of mouse position (center screen)
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out var hit))
+        {
+            if (hit.collider.gameObject.tag == bassTag)
+            {
+                if (Input.GetKeyDown(KeyCode.B))
+                {
+                    spawnBassBall();
+                }
+            }
+        }
+
+    }
+
+    private void spawnBassBall()
+    {
+        float spawnX = 98.0f;
+        float spawnY = 2.0f;
+        float spawnZ = 0.0f;
+
+        Instantiate(spawnee, new Vector3(spawnX, spawnY, spawnZ), Quaternion.identity);
+    }
+}
