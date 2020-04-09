6953b6da6150841882eba3e3ddcbd80cd5d896b6
rfullum <rfullum@gmail.com>
2020-04-09 15:40:54 +0100

    Bass Sequencer Runing
    You can now spawn a bass ball at the bass cylinder, put it in the sequencer, have it spawn, fall, and trigger a wwise event when it hits the platform

--- BassBlockTrigger.cs
+++ BassBlockTrigger.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class BassBlockTrigger : MonoBehaviour
 {
+    [SerializeField] GameObject spawnee;
+
     private int cubeNote;
     private int cubeSequencerPos;
     private string ballTag = "bassSeq";
+
+    private float spawnX;
+    private float spawnY = 10.0f;
+    private float spawnZ;
+
+    MapValues mapValues = new MapValues();
+
 
 
     void Start()
@@ -28,6 +37,14 @@ public class BassBlockTrigger : MonoBehaviour
         string currentCubeNamePos = gameObject.name;
         string cubeSeqPos = currentCubeNamePos.Substring(currentCubeNamePos.Length - 2).Replace("_", "");
         cubeSequencerPos = System.Convert.ToInt32(cubeSeqPos);
+
+        spawnPositionFinder();
+    }
+
+    private void spawnPositionFinder()
+    {
+        spawnX = mapValues.remapValues(cubeSequencerPos, 0.0f, 15.0f, 100.0f, 111.0f);
+        spawnZ = mapValues.remapValues(cubeNote, 0.0f, 24.0f, -25.0f, -15.0f);
     }
 
     private void OnTriggerStay(Collider other)
@@ -40,11 +57,26 @@ public class BassBlockTrigger : MonoBehaviour
 
                 if (beatCount == cubeSequencerPos)
                 {
-                    Debug.Log("POTATO");
+                    spawnBall();
                 }
             }
         }
     }
 
+    private void spawnBall()
+    {
+        spawnee.name = wwiseEventName();
+        Instantiate(spawnee, new Vector3(spawnX, spawnY, spawnZ), Quaternion.identity);
+    }
+
+    private string wwiseEventName()
+    {
+        string[] eventNames = new string[25] { "Bass1C1", "Bass1CshDb1", "Bass1D1", "Bass1DshEb1",
+            "Bass1E1", "Bass1F1", "Bass1FshGb1", "Bass1G1", "Bass1GshAb1", "Bass1A1", "Bass1AshBb1",
+            "Bass1B1", "Bass1C2", "Bass1CshDb2", "Bass1D2", "Bass1DshEb2", "Bass1E2", "Bass1F2",
+            "Bass1FshGb2", "Bass1G2", "Bass1GshAb2", "Bass1A2", "Bass1AshBb2", "Bass1B2", "Bass1C3"};
+
+        return eventNames[cubeNote];
+    }
 
 }
---
+++ TriggerBass.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBass : MonoBehaviour
+{
+    //[SerializeField] string wwiseEvent;
+
+    private string wwiseEvent;
+    private string ballTag = "bassSeq";
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == ballTag)
+        {
+            //wwiseEvent = gameObject.name.Replace("(Clone)", "");
+            wwiseEvent = collision.gameObject.name.Replace("(Clone)", "");
+            AkSoundEngine.PostEvent(wwiseEvent, gameObject);
+            Destroy(collision.gameObject);
+        }
+
+    }
+}
