9c451435f3239345b19865606dcfe6f077962311
rfullum <rfullum@gmail.com>
2020-04-09 15:58:27 +0100

    Update TriggerBass.cs
    Comments

--- TriggerBass.cs
+++ TriggerBass.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class TriggerBass : MonoBehaviour
 {
-    //[SerializeField] string wwiseEvent;
-
     private string wwiseEvent;
     private string ballTag = "bassSeq";
 
+    /**
+     * When ball collides with bass platform, it uses the ball's name to
+     * assign the string to the wwiseEvent, then calls the event, and then
+     * destroys ball.
+     */
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == ballTag)
         {
-            //wwiseEvent = gameObject.name.Replace("(Clone)", "");
             wwiseEvent = collision.gameObject.name.Replace("(Clone)", "");
             AkSoundEngine.PostEvent(wwiseEvent, gameObject);
             Destroy(collision.gameObject);
