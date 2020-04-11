259a7d9c72cd4ebd48cc194f0163b77bc624a4a5
rfullum <rfullum@gmail.com>
2020-04-11 13:16:43 +0100

    Created BassDecay.cs
    Script uses DecayBall position to modify DecayRTPC

---
+++ BassDecay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/**
+ * Sets the bass note decay time via RTPC
+ */
+public class BassDecay : MonoBehaviour
+{
+    MapValues mapValues = new MapValues();
+
+    void Update()
+    {
+        bassDecay();
+    }
+
+    void bassDecay()
+    {
+        var ballPos = transform.position.x;
+        float decayTime = mapValues.remapValues(ballPos, 100.0f, 111.0f, 0.0f, 100.0f);
+        AkSoundEngine.SetRTPCValue("BassDecayTime", decayTime);
+    }
+}
