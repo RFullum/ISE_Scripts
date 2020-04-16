2b8c113d67917f65911e6fc37e94df981c41ebaa
rfullum <rfullum@gmail.com>
2020-04-16 18:38:48 +0100

    Organ Objects Set up
    OrganCubeSpawner spawns an organ cube when raycast + "b". OrganCubeFlier sets OrganCube flying when raycast + "b". OrganPlayer does nothing yet.

---
+++ OrganCubeFlier.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrganCubeFlier : MonoBehaviour
+{
+    private bool activated, isMoving;
+    private Vector3 startPos, targetPos;
+    [SerializeField] float speed;
+
+    /*
+     * Initializes gravity on, activated and isMoving to false,
+     * and startPos to current position
+     */
+    void Start()
+    {
+        GetComponent<Rigidbody>().useGravity = true;
+        activated = isMoving = false;
+        startPos = gameObject.transform.position;
+    }
+
+
+
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        /*
+         * When raycast over cube, and B is pressed, turn off gravity and
+         * set activated to true
+         */
+        if (Physics.Raycast(ray, out var hit))
+        {
+            if (hit.collider.gameObject == gameObject)
+            {
+                if (Input.GetKeyDown(KeyCode.B))
+                {
+                    GetComponent<Rigidbody>().useGravity = false;
+                    activated = true;
+
+                }
+            }
+        }
+
+        if (activated == true)
+        {
+            if(isMoving == true)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, targetPos, step);
+            }
+
+            if (Vector3.Distance(transform.position, targetPos) < 0.1f)
+            {
+                isMoving = false;
+            }
+
+            if (isMoving == false)
+            {
+                moveToTarget();
+            }
+
+        }
+
+    }
+
+    /**
+     * Randomly sets X and Z values for new position movement
+     * Sets isMoving to true
+     */
+    private void moveToTarget()
+    {
+        float xTarget = Random.Range(-150.0f, 150.0f);
+        float yTarget = Random.Range(2.0f, 50.0f);
+        float zTarget = Random.Range(-150.0f, 150.0f);
+
+
+        targetPos = new Vector3(xTarget, yTarget, zTarget);
+        isMoving = true;
+    }
+}
---
+++ OrganCubeSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrganCubeSpawner : MonoBehaviour
+{
+    private string organTag = "organ";
+    [SerializeField] GameObject spawnee;
+
+    private float spawnX = 65.0f;
+    private float spawnY = 10.0f;
+    private float spawnZ = -50.0f;
+
+
+    void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out var hit))
+        {
+            if (hit.collider.gameObject.tag == organTag)
+            {
+                if (Input.GetKeyDown(KeyCode.B))
+                {
+                    spawnOrganCube();
+                }
+            }
+        }
+    }
+
+    private void spawnOrganCube()
+    {
+        Instantiate(spawnee, new Vector3(spawnX, spawnY, spawnZ), Quaternion.identity);
+    }
+}
