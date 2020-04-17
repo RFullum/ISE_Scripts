53ec33c0168de513ab5c07f213e74d54d7a8f104
rfullum <rfullum@gmail.com>
2020-04-17 18:13:37 +0100

    Reverb RTPC Controls
    Control the Snare Verb Amount and Decay, and the instrument verb amount and decay by moving spheres around

---
+++ InstrumentVerbAmt.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstrumentVerbAmt : MonoBehaviour
+{
+    private float verbAmt, verbDecay;
+    private float colorR, colorG;
+    MapValues mapValues = new MapValues();
+
+    /*
+     * Updates instrument verb amount based on X position value
+     * Updates instrument verb decay based on Z position value
+     * Changes color based on X and Z positions
+     */
+    void Update()
+    {
+        var renderer = GetComponent<Renderer>();
+        float xPos = gameObject.transform.position.x;
+        float zPos = gameObject.transform.position.z;
+
+        verbAmt = mapValues.remapValues(xPos, -26.0f, 147.0f, 0.0f, 100.0f);
+        verbDecay = mapValues.remapValues(zPos, -99.0f, 99.0f, 0.0f, 100.0f);
+        AkSoundEngine.SetRTPCValue("VerbSendAmt", verbAmt);
+        AkSoundEngine.SetRTPCValue("VerbDecay", verbDecay);
+
+        // X value changes Red value
+        // Z value changes Green value
+        colorR = mapValues.remapValues(verbAmt, 0.0f, 100.0f, 0.0f, 1.0f);
+        colorG = mapValues.remapValues(verbDecay, 0.0f, 100.0f, 0.0f, 1.0f);
+        renderer.material.SetColor("_Color", new Color(colorR, colorG, 1.0f, 1.0f));
+
+    }
+}
---
+++ SnareVerbRTPC.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnareVerbRTPC : MonoBehaviour
+{
+    private float verbAmt, verbDecay;
+    private float colorG, colorB;
+    MapValues mapValues = new MapValues();
+
+    /*
+     * Updates snare verb amount based on X position value
+     * updates snare verb decay based on Z position value
+     * changes colors based on position
+     */
+    void Update()
+    {
+        var renderer = GetComponent<Renderer>();
+        float xPos = gameObject.transform.position.x;
+        float zPos = gameObject.transform.position.z;
+
+        verbAmt = mapValues.remapValues(xPos, -26.0f, 147.0f, 0.0f, 100.0f);
+        verbDecay = mapValues.remapValues(zPos, -99.0f, 99.0f, 0.0f, 100.0f);
+
+        AkSoundEngine.SetRTPCValue("SnrVerbAmt", verbAmt);
+        AkSoundEngine.SetRTPCValue("SnrVerbDecay", verbDecay);
+
+        // X value changes Green value
+        // Z value changes Blue value
+        colorG = mapValues.remapValues(verbAmt, 0.0f, 100.0f, 0.0f, 1.0f);
+        colorB = mapValues.remapValues(verbDecay, 0.0f, 100.0f, 0.0f, 1.0f);
+        renderer.material.SetColor("_Color", new Color(1.0f, colorG, colorB, 1.0f));
+    }
+}
