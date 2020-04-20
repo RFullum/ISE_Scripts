e9f7e2101106aa1dbd86b08abea78201e46ba6e6
rfullum <rfullum@gmail.com>
2020-04-20 18:45:37 +0100

    PlayTrack.cs
    Plays song when you jump

---
+++ PlayTrack.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayTrack : MonoBehaviour
+{
+    [SerializeField] string wwiseEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            AkSoundEngine.PostEvent(wwiseEvent, gameObject);
+        }
+    }
+}
