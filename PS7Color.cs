9ec8e3bc441bb265e4caf327d51f42f0362ae35b
rfullum <rfullum@gmail.com>
2020-04-21 20:59:50 +0100

    PS7Color.cs
    Started setting up color script for particle system 7

---
+++ PS7Color.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PS7Color : MonoBehaviour
+{
+    private ParticleSystem ps;
+    private GameObject player, player2;
+
+
+    MapValues mapValues = new MapValues();
+
+    uint playingID = (uint)AkQueryRTPCValue.RTPCValue_PlayingID;
+    public static float lowMeter, lowMidMeter, hiMidMeter, hiMeter;
+    [SerializeField] GameObject obj;
+
+
+    void Start()
+    {
+        // Sets GameObjects
+        player = GameObject.Find("FPSController");
+        player2 = GameObject.Find("FPSController/FirstPersonCharacter");
+
+        ps = GetComponent<ParticleSystem>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        getMeterValues();
+    }
+
+    private void getMeterValues()
+    {
+        int type = 1;
+        AkSoundEngine.GetRTPCValue("DRMLowMeter", obj, playingID, out lowMeter, ref type);
+        AkSoundEngine.GetRTPCValue("DRMLowMidMeter", obj, playingID, out lowMidMeter, ref type);
+        AkSoundEngine.GetRTPCValue("DRMHiMidMeter", obj, playingID, out hiMidMeter, ref type);
+        AkSoundEngine.GetRTPCValue("DRMHiMeter", obj, playingID, out hiMeter, ref type);
+    }
+}
