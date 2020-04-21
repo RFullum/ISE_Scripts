cfa1bc383be9328249ae652ea881c8ff53904550
rfullum <rfullum@gmail.com>
2020-04-21 20:42:01 +0100

    PSSelfDestruct.cs
    Particle Systems destroy themselves after a time

---
+++ PSSelfDestruct.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PSSelfDestruct : MonoBehaviour
+{
+    private float lifeTime = 0.0f;
+    private float selfDestruct = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lifeTime > selfDestruct)
+        {
+            Destroy(gameObject);
+        }
+        lifeTime += Time.deltaTime;
+    }
+}
