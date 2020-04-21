620815f12152aef02edd9a6824be22a596892d4d
rfullum <rfullum@gmail.com>
2020-04-21 18:57:47 +0100

    Update PS3Color.cs
    Meter RTPC values control more aspects of the system

--- PS3Color.cs
+++ PS3Color.cs
@@ -7,11 +7,17 @@ using UnityEngine;
  */
 public class PS3Color : MonoBehaviour
 {
+    // Particle system and player variables
     private ParticleSystem ps;
     private GameObject player, player2;
     private float posX, posY, posZ, rotX, rotY, tester;
 
     MapValues mapValues = new MapValues();
+
+    // RTPC Meter value variables
+    uint playingID = (uint)AkQueryRTPCValue.RTPCValue_PlayingID;
+    public static float lowMeter, lowMidMeter, hiMidMeter, hiMeter;
+    [SerializeField] GameObject obj;
 
 
     void Start()
@@ -27,6 +33,8 @@ public class PS3Color : MonoBehaviour
 
     void Update()
     {
+        getMeterValues();
+
         // Gets position values from Player
         posX = player.transform.position.x;
         posY = player.transform.position.y;
@@ -43,7 +51,23 @@ public class PS3Color : MonoBehaviour
         var main = ps.main;
         main.startColor = new Color(rVal, gVal, bVal, 1.0f);
 
+        float trailWidth = mapValues.remapValues(lowMeter, -48.0f, 0.0f, 3.0f, 10.0f);
+        float lifeColor1 = mapValues.remapValues(hiMidMeter, -48.0f, 0.0f, 0.0f, 1.0f);
+        float lifeColor2 = mapValues.remapValues(lowMeter, -48.0f, 0.0f, 0.76f, 0.12f);
+
         var trails = ps.trails;
         trails.inheritParticleColor = true;
+        trails.widthOverTrail = trailWidth;
+        trails.colorOverLifetime = new Color(0.2f, lifeColor1, lifeColor2, 1.0f);
+        trails.colorOverTrail = new Color(0.2f, lifeColor2, lifeColor1, 1.0f);
+    }
+
+    private void getMeterValues()
+    {
+        int type = 1;
+        AkSoundEngine.GetRTPCValue("DRMLowMeter", obj, playingID, out lowMeter, ref type);
+        AkSoundEngine.GetRTPCValue("DRMLowMidMeter", obj, playingID, out lowMidMeter, ref type);
+        AkSoundEngine.GetRTPCValue("DRMHiMidMeter", obj, playingID, out hiMidMeter, ref type);
+        AkSoundEngine.GetRTPCValue("DRMHiMeter", obj, playingID, out hiMeter, ref type);
     }
 }
