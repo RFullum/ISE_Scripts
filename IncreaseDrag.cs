2b6873aa4a9b01aa3683af9368479efc42894232
rfullum <rfullum@gmail.com>
2020-04-22 11:24:06 +0100

    Update IncreaseDrag.cs
    increased drag

--- IncreaseDrag.cs
+++ IncreaseDrag.cs
@@ -28,6 +28,6 @@ public class IncreaseDrag : MonoBehaviour
             rb.drag = dragAmt * dragMult;
         }
 
-        dragMult = (Time.deltaTime + dragMult) * 2.0f;
+        dragMult = (Time.deltaTime + dragMult) * 10.0f;
     }
 }
